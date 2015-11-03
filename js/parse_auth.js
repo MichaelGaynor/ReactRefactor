7929b250c84834c805253dde86e3c96283fbc942
Michael Gaynor <jmichaelgaynor@gmail.com>
2015-11-03 12:33:24 -0500

    Initial Commit

--- js/parse_auth.js
+++ js/parse_auth.js
@@ -1,6 +1,6 @@
-const APP_ID = 'mmoMgOQzCeRE8Ad4vmRkHMLYyTwEPPrAGXMEfDFm';
-const API_KEY = 'xvocUSdI55mrUV7m7fb0ylyXO2kQ6EML2mlBDEoY';
-const APP_URL = 'https://api.parse.com/1/classes/Todo';
+const APP_ID = 'iANIHMWO4db2LaXZK8Z2nYOm9qkVUpsM7v9QbZKJ';
+const API_KEY = 'GYT2gHyJr8ySBZbY2Jmb6EPf0z9wRtJhkOQitMTR';
+const APP_URL = 'https://api.parse.com/1/classes/ReactRefactor';
 
 export default {
   APP_ID,
